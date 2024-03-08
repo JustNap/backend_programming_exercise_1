3fe8d698dc0260504e8bb0ba33743f4128539c0c
JustNap <justinvalen04@gmail.com>
2024-03-08 20:11:42 +0700

    535230176 Justin Valencius Cahyadi

---
+++ Soal1.js
@@ -0,0 +1,28 @@
+function prima(angka){
+    if (angka <= 1){
+        return false;
+    }
+    if (angka <= 3){
+        return true;
+    }
+    
+    if(angka % 2 === 0 || angka % 3 === 0){
+        return false;
+    }
+
+    let i = 5;
+    while (i * i <= angka){
+        if (angka % i === 0 || angka % (i + 2) === 0){
+            return false;
+        }
+        i += 6;
+    }
+
+    return true;
+}
+
+for(let i = 1; i <= 1000; i++){
+    if(angka(i)){
+        console.log(i);
+    }
+}
